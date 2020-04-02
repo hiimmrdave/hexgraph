e0eb0d6bcb3659915ed1512a3f11d1b9cec9e53e
David Vogt <horsedreamer@liam.local>
2020-04-02 10:44:22 -0400

    made testing more complete, can't compare sets

--- test/celltests.ts
+++ test/celltests.ts
@@ -3,10 +3,9 @@ import { it } from "mocha";
 import * as Cell from "../src/cell";
 import { areEqual } from "../src/main";
 import { NodeType } from "../src/types";
-import { makeEdge } from "../src/edge"
+import { makeEdge } from "../src/edge";
 
-describe("Cell properties", function () {
-
+describe("Cell properties", function() {
   const origin = Object.freeze(Cell.makeCell({ q: 0, r: 0, s: 0 }));
 
   it("cell type should be Cell", () => {
@@ -18,21 +17,65 @@ describe("Cell properties", function () {
   });
 
   it("diagonals to origin should equal DIAGONALS", () => {
-    let subject = Cell.diagonals(origin).map(e=>e.id);
-    let result = Cell.DIAGONALS.map(e=>Cell.makeCell(e).id);
-    expect(subject).to.deep.equal(result,"Diagonals are wrong");
+    let subject = Cell.diagonals(origin).map(({ id, q, r, s, nodetype }) => ({
+      id,
+      q,
+      r,
+      s,
+      nodetype
+    }));
+    let result = Cell.DIAGONALS.map(e => {
+      const each = Cell.makeCell(e);
+      return {
+        id: each.id,
+        q: each.q,
+        r: each.r,
+        s: each.s,
+        nodetype: each.nodetype
+      };
+    });
+    expect(subject).to.deep.equal(result, "Diagonals are wrong");
   });
 
-  it("neighbors to origin should equal DIRECTIONS", () =>{
-    let subject = Cell.cells(origin).map(e=>e.id);
-    let result = Cell.DIRECTIONS.map(e=>Cell.makeCell(e).id);
-    expect(subject).to.deep.equal(result,"Directions are wrong");
+  it("neighbors to origin should equal DIRECTIONS", () => {
+    let subject = Cell.cells(origin).map(({ id, q, r, s, nodetype }) => ({
+      id,
+      q,
+      r,
+      s,
+      nodetype
+    }));
+    let result = Cell.DIRECTIONS.map(e => {
+      const each = Cell.makeCell(e);
+      return {
+        id: each.id,
+        q: each.q,
+        r: each.r,
+        s: each.s,
+        nodetype: each.nodetype
+      };
+    });
+    expect(subject).to.deep.equal(result, "Directions are wrong");
   });
 
-  it("edges should be half the coordinates of the edges",() =>{
-    let subject = Cell.edges(origin).map(e=>e.id);
-    let result = Cell.DIRECTIONS.map((e)=>makeEdge(Cell.multiply(e,0.5)).id);
-    expect(subject).to.deep.equal(result,"Edges are wrong");
+  it("edges should be half the coordinates of the edges", () => {
+    let subject = Cell.edges(origin).map(({ id, q, r, s, nodetype }) => ({
+      id,
+      q,
+      r,
+      s,
+      nodetype
+    }));
+    let result = Cell.DIRECTIONS.map(e => {
+      const each = makeEdge(Cell.multiply(e, 0.5));
+      return {
+        id: each.id,
+        q: each.q,
+        r: each.r,
+        s: each.s,
+        nodetype: each.nodetype
+      };
+    });
+    expect(subject).to.deep.equal(result, "Edges are wrong");
   });
-
 });
