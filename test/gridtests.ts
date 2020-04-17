035f7e0c2cde2528b9981aea65051596d59b7055
David Vogt <horsedreamer@liam.local>
2020-04-16 22:48:18 -0400

    write failing tests

---
+++ test/gridtests.ts
@@ -0,0 +1,55 @@
+import { expect } from "chai";
+import { it } from "mocha";
+import { CartesianVector, GridShape } from "../src/types";
+import * as hex from "../src/hex";
+import * as grid from "../src/grid";
+
+describe("grid functions", function() {
+  const size: CartesianVector = { x: 3, y: 5 };
+
+  it("start with a hex grid", () => {
+    const hexgrid = grid.make({
+      shape: GridShape.Hexagon,
+      size,
+      populate: true,
+    });
+    hexgrid.forEach((val, key) => console.log(`${val.nodetype}:\t${key}`));
+    expect(hexgrid.size).to.equal(226);
+  });
+
+  it("start with a triangle grid", () => {
+    const hexgrid = grid.make({
+      shape: GridShape.Triangle,
+      size,
+      populate: true,
+    });
+    hexgrid.forEach((val, key) => console.log(`${val.nodetype}:\t${key}`));
+    expect(hexgrid.size).to.equal(226);
+  });
+
+  it("start with a star grid", () => {
+    const hexgrid = grid.make({ shape: GridShape.Star, size, populate: true });
+    hexgrid.forEach((val, key) => console.log(`${val.nodetype}:\t${key}`));
+    expect(hexgrid.size).to.equal(226);
+  });
+
+  it("start with a parallelogram grid", () => {
+    const hexgrid = grid.make({
+      shape: GridShape.Parallelogram,
+      size,
+      populate: true,
+    });
+    hexgrid.forEach((val, key) => console.log(`${val.nodetype}:\t${key}`));
+    expect(hexgrid.size).to.equal(226);
+  });
+
+  it("start with a rectangle grid", () => {
+    const hexgrid = grid.make({
+      shape: GridShape.Rectangle,
+      size,
+      populate: true,
+    });
+    hexgrid.forEach((val, key) => console.log(`${val.nodetype}:\t${key}`));
+    expect(hexgrid.size).to.equal(226);
+  });
+});
