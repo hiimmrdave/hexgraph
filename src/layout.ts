1fbf24e04ae3c9f1bb8d4f3e96fa8f98a947c5f5
David Vogt <horsedreamer@liam.local>
2020-10-03 13:22:48 -0400

    compose matrices, reduce arrays by composition

--- src/layout.ts
+++ src/layout.ts
@@ -14,98 +14,100 @@ export type XYVector = {
 
 /** a 2x2 matrix */
 type Matrix2x2 = [[number, number], [number, number]];
-/** a matrix to convert between QRS and XY space */
-type TransformMatrix = {
-  f: Matrix2x2;
-  b: Matrix2x2;
-};
 
 /**
  * a set of values to convert from CubeVector grid coordinates to
  * CartesianVector screen coodrinates
  */
 export type LayoutConfig = {
-  radius: XYVector;
-  origin: XYVector;
-  size: XYVector;
+  readonly radius: XYVector;
+  readonly origin: XYVector;
+  readonly size: XYVector;
+  readonly cubeToPoint: Matrix2x2;
+  readonly pointToCube: Matrix2x2;
 };
 
-const qrxy: TransformMatrix = {
-  f: [
+const QRXY: Matrix2x2 = [
     [3 / 2, 0],
     [SQRT_THREE / 2, SQRT_THREE],
   ],
-  b: [
+  /*  XYQR: Matrix2x2 = [
     [2 / 3, 0],
     [-1 / 3, SQRT_THREE / 3],
-  ],
-};
+  ],*/
+  Identity2x2: Matrix2x2 = [
+    [1, 0],
+    [0, 1],
+  ];
 
-export function rotate(theta: number): TransformMatrix {
-  return {
-    f: [
-      [Math.cos(theta), -Math.sin(theta) + 0],
-      [Math.sin(theta), Math.cos(theta)],
-    ],
-    b: [
-      [Math.cos(theta), Math.sin(theta)],
-      [-Math.sin(theta) + 0, Math.cos(theta)],
-    ],
-  };
-}
-
-function invertMatrix([[A, B], [C, D]]: Matrix2x2): Matrix2x2 {
+export function rotate(theta: number): Matrix2x2 {
   return [
-    [D / (A * D - B * C), B / (B * C - A * C)],
-    [C / (B * C - A * C), A / (A * D - B * C)],
+    [Math.cos(theta) + 0, -Math.sin(theta) + 0],
+    [Math.sin(theta) + 0, Math.cos(theta) + 0],
   ];
 }
 
-function composeMatrices(
-  { f: [[A, B], [C, D]] }: TransformMatrix,
-  { f: [[E, F], [G, H]] }: TransformMatrix
-): TransformMatrix {
-  return {
-    f: [
-      [A * E + B * G, A * F + B * H],
-      [C * E + D * G, C * F + D * H],
-    ],
-    b: invertMatrix([
-      [A * E + B * G, A * F + B * H],
-      [C * E + D * G, C * F + D * H],
-    ]),
-  };
+function invertMatrix2x2([[a, b], [c, d]]: Matrix2x2): Matrix2x2 {
+  return [
+    [d / (a * d - b * c), b / (b * c - a * c)],
+    [c / (b * c - a * c), a / (a * d - b * c)],
+  ];
 }
 
-console.log(composeMatrices(qrxy, rotate(Math.PI / 2)));
+function composeMatrices2x2(
+  [[a, b], [c, d]]: Matrix2x2,
+  [[e, f], [g, h]]: Matrix2x2
+): Matrix2x2 {
+  return [
+    [a * e + b * g, a * f + b * h],
+    [c * e + d * g, c * f + d * h],
+  ];
+}
+
+function composeMatrixArray(matrices: Matrix2x2[]): Matrix2x2 {
+  return matrices.reduce(
+    (acc, cur) => composeMatrices2x2(acc, cur),
+    Identity2x2
+  );
+}
 
 export function configureLayout(
   radius: XYVector,
   origin: XYVector,
-  size: XYVector
+  size: XYVector,
+  transforms: Matrix2x2[] = []
 ): LayoutConfig {
-  return { radius, origin, size };
+  const cubeToPoint = composeMatrixArray([QRXY, ...transforms]),
+    pointToCube = invertMatrix2x2(cubeToPoint);
+  return {
+    radius,
+    origin,
+    size,
+    cubeToPoint,
+    pointToCube,
+  };
 }
 
 export function cubeToPoint(
   c: QRSVector,
-  { radius, origin }: LayoutConfig
+  { radius, origin, cubeToPoint: M }: LayoutConfig
 ): XYVector {
-  const x = (qrxy.f[0][0] * c.q + qrxy.f[0][1] * c.r) * radius.x + origin.x,
-    y = (qrxy.f[1][0] * c.q + qrxy.f[1][1] * c.r) * radius.y + origin.y;
-  return { x: thousandthRound(x), y: thousandthRound(y) };
+  const x =
+      thousandthRound(M[0][0] * c.q + M[0][1] * c.r) * radius.x + origin.x,
+    y = thousandthRound(M[1][0] * c.q + M[1][1] * c.r) * radius.y + origin.y;
+  return { x, y };
 }
 
 export function pointToCube(
   p: XYVector,
-  { radius, origin }: LayoutConfig
+  { radius, origin, pointToCube: M }: LayoutConfig
 ): QRSVector {
   const pt = {
       x: (p.x - origin.x) / radius.x,
       y: (p.y - origin.y) / radius.y,
     },
-    q = qrxy.b[0][0] * pt.x + qrxy.b[0][1] * pt.y,
-    r = qrxy.b[1][0] * pt.x + qrxy.b[1][1] * pt.y,
+    q = M[0][0] * pt.x + M[0][1] * pt.y,
+    r = M[1][0] * pt.x + M[1][1] * pt.y,
     s = -q - r;
   return { q, r, s };
 }
